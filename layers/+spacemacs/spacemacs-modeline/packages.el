smile13241324/spacemacs

commit a015ba20f687782e949496d86440190131036f83

Reduce installation of packages that won't be used (#16737)
* Use :toggle rather than :if where possible

The former prevents packages from being installed if they are not
needed; for example, if the osx layer is declared but the current
machine is not running osx, the packages shouldn't even be installed.

* Remove use-package :if expressions for packages with equivalent :toggle

These init functions will never run into the case where the :if
expression returns nil.

* Do not install flycheck-* packages if no syntax-checking layer

* Replace :toggle with equivalent :requires where possible

layers/+spacemacs/spacemacs-modeline/packages.el
@@ -25,7 +25,8 @@
       '(
         (doom-modeline :toggle (eq (spacemacs/get-mode-line-theme-name) 'doom))
         fancy-battery
-        spaceline
+        (spaceline :toggle (memq (spacemacs/get-mode-line-theme-name)
+                                 '(spacemacs all-the-icons custom)))
         (spaceline-all-the-icons :toggle (eq (spacemacs/get-mode-line-theme-name) 'all-the-icons))
         symon
         (vim-powerline :location (recipe :fetcher local))))
@@ -51,8 +52,6 @@
 
 (defun spacemacs-modeline/init-spaceline ()
   (use-package spaceline-config
-    :if (memq (spacemacs/get-mode-line-theme-name)
-              '(spacemacs all-the-icons custom))
     :init
     (spacemacs|require-when-dumping 'spaceline)
     (spacemacs|when-dumping-strict

layers/+checkers/spell-checking/packages.el
@@ -22,7 +22,7 @@
 
 (defconst spell-checking-packages
   '(
-    auto-dictionary
+    (auto-dictionary :toggle spell-checking-enable-auto-dictionary)
     flyspell
     flyspell-correct
     (flyspell-correct-ivy :toggle (configuration-layer/layer-used-p 'ivy))
@@ -34,7 +34,6 @@
 (defun spell-checking/init-auto-dictionary ()
   (use-package auto-dictionary
     :defer t
-    :if spell-checking-enable-auto-dictionary
     :init
     (add-hook 'flyspell-mode-hook 'auto-dictionary-mode)
     ;; Select the buffer local dictionary if it was set, otherwise

layers/+checkers/syntax-checking/packages.el
@@ -28,7 +28,7 @@
 (defconst syntax-checking-packages
   '(
     flycheck
-    flycheck-pos-tip
+    (flycheck-pos-tip :toggle syntax-checking-enable-tooltips)
     popwin))
 
 (defun syntax-checking/init-flycheck ()
@@ -79,7 +79,6 @@
 
 (defun syntax-checking/init-flycheck-pos-tip ()
   (use-package flycheck-pos-tip
-    :if syntax-checking-enable-tooltips
     :after (flycheck)
     :init
     (flycheck-pos-tip-mode)

layers/+filetree/treemacs/packages.el
@@ -91,8 +91,7 @@
 
 (defun treemacs/init-treemacs-evil ()
   (use-package treemacs-evil
-    :after treemacs
-    :if (memq dotspacemacs-editing-style '(vim hybrid))))
+    :after treemacs))
 
 (defun treemacs/init-treemacs-projectile ()
   (use-package treemacs-projectile
@@ -112,7 +111,6 @@
 
 (defun treemacs/init-treemacs-all-the-icons ()
   (use-package treemacs-all-the-icons
-    :if treemacs-use-all-the-icons-theme
     :hook ((treemacs-mode dired-mode) . (lambda () (treemacs-load-theme 'all-the-icons)))))
 
 (defun treemacs/pre-init-winum ()

layers/+intl/chinese/packages.el
@@ -50,7 +50,6 @@
 
 (defun chinese/init-pyim ()
   (use-package pyim
-    :if chinese-default-input-method
     :defer t
     :init
     (setq pyim-directory (expand-file-name "pyim/" spacemacs-cache-directory)
@@ -66,7 +65,6 @@
 (defun chinese/init-pyim-basedict ()
   "Initialize pyim-basedict"
   (use-package pyim-basedict
-    :if (eq chinese-default-input-method 'pinyin)
     :defer t
     :config
     (pyim-basedict-enable)))
@@ -74,7 +72,6 @@
 (defun chinese/init-pyim-wbdict ()
   "Initialize pyim-wbdict"
   (use-package pyim-wbdict
-    :if (member chinese-default-input-method '(wubi wubi86 wubi98))
     :defer t
     :config
     (setq pyim-default-scheme 'wubi)
@@ -84,7 +81,6 @@
 
 (defun chinese/init-youdao-dictionary ()
   (use-package youdao-dictionary
-    :if chinese-enable-youdao-dict
     :defer t
     :config
     ;; Enable Cache

layers/+lang/clojure/packages.el
@@ -33,13 +33,16 @@
     eldoc
     evil-cleverparens
     flycheck
-    (flycheck-clojure :toggle (memq 'squiggly (if (listp clojure-enable-linters)
+    (flycheck-clojure :requires flycheck
+                      :toggle (memq 'squiggly (if (listp clojure-enable-linters)
                                                   clojure-enable-linters
                                                 (list clojure-enable-linters))))
-    (flycheck-clj-kondo :toggle (memq 'clj-kondo (if (listp clojure-enable-linters)
+    (flycheck-clj-kondo :requires flycheck
+                        :toggle (memq 'clj-kondo (if (listp clojure-enable-linters)
                                                      clojure-enable-linters
                                                    (list clojure-enable-linters))))
-    (flycheck-joker :toggle (memq 'joker (if (listp clojure-enable-linters)
+    (flycheck-joker :requires flycheck
+                    :toggle (memq 'joker (if (listp clojure-enable-linters)
                                              clojure-enable-linters
                                            (list clojure-enable-linters))))
     ggtags
@@ -607,16 +610,13 @@
 
 (defun clojure/init-flycheck-clojure ()
   (use-package flycheck-clojure
-    :if (configuration-layer/package-usedp 'flycheck)
     :config
     (flycheck-clojure-setup)
     (with-eval-after-load 'cider
       (flycheck-clojure-inject-jack-in-dependencies))))
 
 (defun clojure/init-flycheck-clj-kondo ()
-  (use-package flycheck-clj-kondo
-    :if (configuration-layer/package-usedp 'flycheck)))
+  (use-package flycheck-clj-kondo))
 
 (defun clojure/init-flycheck-joker ()
-  (use-package flycheck-joker
-    :if (configuration-layer/package-usedp 'flycheck)))
+  (use-package flycheck-joker))

layers/+lang/elixir/packages.el
@@ -23,7 +23,7 @@
 
 (defconst elixir-packages
   '(
-    alchemist
+    (alchemist :toggle (eq elixir-backend 'alchemist))
     company
     dap-mode
     elixir-mode
@@ -37,7 +37,6 @@
 
 (defun elixir/init-alchemist ()
   (use-package alchemist
-    :if (eq elixir-backend 'alchemist)
     :defer t
     :init
     (spacemacs/register-repl 'alchemist 'alchemist-iex-run "alchemist")

layers/+lang/emacs-lisp/packages.el
@@ -36,8 +36,8 @@
     evil-cleverparens
     eval-sexp-fu
     flycheck
-    flycheck-elsa
-    flycheck-package
+    (flycheck-elsa :requires flycheck)
+    (flycheck-package :requires flycheck)
     ggtags
     (ielm :location built-in)
     (inspector :location (recipe

commit c5eeb3239607069bf592632913bd495fe55be1e5

[modeline] Fix vim-powerline theme erroring on startup
This regressed in #16737.  vim-powerline depends on powerline which
happened to get installed as a dependency of spaceline, but
vim-powerline does not depend on spaceline itself so it should not
cause spaceline to get installed.

While we're at it, don't install vim-powerline unless that mode line
theme is actually used.

layers/+spacemacs/spacemacs-modeline/packages.el
@@ -29,7 +29,9 @@
                                  '(spacemacs all-the-icons custom)))
         (spaceline-all-the-icons :toggle (eq (spacemacs/get-mode-line-theme-name) 'all-the-icons))
         symon
-        (vim-powerline :location (recipe :fetcher local))))
+        (powerline :toggle (eq (spacemacs/get-mode-line-theme-name) 'vim-powerline))
+        (vim-powerline :location (recipe :fetcher local)
+                       :toggle (eq (spacemacs/get-mode-line-theme-name) 'vim-powerline))))
 
 (defun spacemacs-modeline/init-doom-modeline ()
   ;; doom modeline depends on `display-graphic-p' so we delay its initialization
@@ -186,27 +188,28 @@
       :documentation "Tiny graphical system monitor."
       :evil-leader "tms")))
 
+(defun spacemacs-modeline/init-powerline ())
+
 (defun spacemacs-modeline/init-vim-powerline ()
-  (when (eq 'vim-powerline (spacemacs/get-mode-line-theme-name))
-    (require 'powerline)
-    (if (display-graphic-p)
-        (setq powerline-default-separator 'arrow)
-      (setq powerline-default-separator 'utf-8))
-    (defun powerline-raw (str &optional face pad)
-      "Render STR as mode-line data using FACE and optionally
+  (require 'powerline)
+  (if (display-graphic-p)
+      (setq powerline-default-separator 'arrow)
+    (setq powerline-default-separator 'utf-8))
+  (defun powerline-raw (str &optional face pad)
+    "Render STR as mode-line data using FACE and optionally
 PAD import on left (l) or right (r) or left-right (lr)."
-      (when str
-        (let* ((rendered-str (format-mode-line str))
-               (padded-str (concat
-                            (when (and (> (length rendered-str) 0)
-                                       (or (eq pad 'l) (eq pad 'lr))) " ")
-                            (if (listp str) rendered-str str)
-                            (when (and (> (length rendered-str) 0)
-                                       (or (eq pad 'r) (eq pad 'lr))) " "))))
-          (if face
-              (pl/add-text-property padded-str 'face face)
-            padded-str))))
-    (require 'vim-powerline-theme)
-    (powerline-vimish-theme)
-    (add-hook 'emacs-startup-hook
-              'spacemacs//set-vimish-powerline-for-startup-buffers)))
+    (when str
+      (let* ((rendered-str (format-mode-line str))
+             (padded-str (concat
+                          (when (and (> (length rendered-str) 0)
+                                     (or (eq pad 'l) (eq pad 'lr))) " ")
+                          (if (listp str) rendered-str str)
+                          (when (and (> (length rendered-str) 0)
+                                     (or (eq pad 'r) (eq pad 'lr))) " "))))
+        (if face
+            (pl/add-text-property padded-str 'face face)
+          padded-str))))
+  (require 'vim-powerline-theme)
+  (powerline-vimish-theme)
+  (add-hook 'emacs-startup-hook
+            'spacemacs//set-vimish-powerline-for-startup-buffers))